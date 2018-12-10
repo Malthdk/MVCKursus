6689632114d497cb4ebc468024be7b53229dce02
Malte Holledig <malte@holledig.dk>
2018-12-10 14:06:37 +0100

    Intro app til MVC kursus

---
+++ 10-12_Intro/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _10_12_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+
+            string navn = "Malte";
+
+            return Json(navn, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ 10-12_Intro/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_10_12_Intro.MvcApplication" Language="C#" %>
