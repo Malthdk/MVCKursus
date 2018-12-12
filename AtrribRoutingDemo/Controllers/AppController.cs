d5b06e0c44e1cba18a66d7c8b5930d4904bf31dc
Malte Holledig <malte@holledig.dk>
2018-12-12 12:33:50 +0100

    Routing attributes demo

---
+++ AtrribRoutingDemo/Controllers/AppController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AtrribRoutingDemo.Controllers
+{
+    public class AppController : Controller
+    {
+        [HttpGet]
+        [Route("~/")]
+        [Route("~/home")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("~/Salg/{kundeid:int}")]
+        public ActionResult Salg(int kundeid)
+        {
+            ViewBag.kundeid = kundeid;
+            return View();
+        }
+    }
+}
