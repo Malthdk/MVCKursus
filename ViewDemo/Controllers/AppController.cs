e9e282b621fb532964a0e1bc0602d0ba5d0bcefb
Malte Holledig <malte@holledig.dk>
2018-12-12 13:31:07 +0100

    View html/c# legeplads

---
+++ ViewDemo/Controllers/AppController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewDemo.Controllers
+{
+    public class AppController : Controller
+    {
+        [HttpGet]
+        [Route("~/")]
+        public ActionResult Index()
+        {
+            List<string> tmp = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                tmp.Add(Guid.NewGuid().ToString());
+            }
+            return View(tmp);
+        }
+    }
+}
