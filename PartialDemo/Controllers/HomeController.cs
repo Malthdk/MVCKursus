af6bef8edd976ace2cbe64de1e56b26553282638
Malte Holledig <malte@holledig.dk>
2018-12-12 10:25:02 +0100

    Partial demo

---
+++ PartialDemo/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [ChildActionOnly]
+        public ActionResult Liste()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        [ChildActionOnly]
+        public ActionResult Liste2(int antal = 10)
+        {
+            try
+            {
+                List<string> lst = new List<string>();
+
+                for (int i = 0; i < antal; i++)
+                {
+                    lst.Add(Guid.NewGuid().ToString());
+                }
+
+                return PartialView(lst);
+            }
+            catch (Exception ex)
+            {
+
+                return RedirectToAction("Error");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Error()
+        {
+           return View();
+        }
+    }
+}
